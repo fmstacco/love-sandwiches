ad465246098793a33d3e51d749b94437e210c359
fmstacco <fmstacco@gmail.com>
2022-07-21 08:32:04 +0000

    Request data from worksheet, build dictionary and print the dictionary to the terminal

--- run.py
+++ run.py
@@ -130,8 +130,25 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
+
+
+def get_stock_values(data):
+    
+    headings = SHEET.worksheet("stock").row_values(1)
+    
+    stock_dict = {key: value for key, value in zip(headings, data)}
+    
+    return stock_dict
+    
+
+stock_values = get_stock_values(stock_data)
+
+
+print("Make the following numbers of sandwiches for next market:")
+print(stock_values)
 
